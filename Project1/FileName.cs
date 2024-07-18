b5d941589be197d8e830bc4a09f9f55cae9321db
sivapalagarla <palagarlait3@gmail.com>
2024-07-19 00:42:25 +0530

    siva changes

---
+++ Project1/FileName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Project1
+{
+    internal class FileName
+    {
+
+        string ptrtr = string.Empty;
+        double width, length, wallArea, ceilingArea;
+        string widthString, lengthString;
+        double HEIGHT = 8;
+
+        Console.WriteLine( "Calculation of Room Paint Requirements");
+      Console.Write( "Enter room length: ");
+      lengthString = Console.ReadLine();
+      length = double.Parse(lengthString);
+        Console.Write( "Enter room width: ");
+      widthString = Console.ReadLine();
+      width = double.Parse(widthString);
+
+
+    }
+}
